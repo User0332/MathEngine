7caeb42e0b4d921cc3894dc57bf9c42157b488cf
Carl Furtado <carlzfurtado@gmail.com>
2025-03-03 18:05:33 -0500

    fix chain ruling

--- src/MathEngine.Tests/BasicDerivativeTest.cs
+++ src/MathEngine.Tests/BasicDerivativeTest.cs
@@ -1,5 +1,7 @@
+using MathEngine.Algebra;
 using MathEngine.Calculus.Univariate.Differential;
 using MathEngine.Trig.CalcPlugin;
+using MathEngine.Trig.Functions;
 
 namespace MathEngine.Tests;
 
@@ -7,10 +9,16 @@ public static class BasicDerivativeTest
 {
 	public static void Run()
 	{
+		Variable x = Variable.X;
+
 		Differentiator differentiator = new();
 		
 		differentiator.AddPlugin<TrignometricDerivativeInfo>();
 
-		
+		var func = TrigFunctions.Sin.ValueAt(x^2);
+
+		var derivative = differentiator.Differentiate(func, x).Simplify();
+
+		Console.WriteLine(derivative);
 	}
 }
