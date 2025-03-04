606d0886cb603653dcc563d587d83b8fe5ff9295
User0332 <carlzfurtado@gmail.com>
2025-03-04 08:38:55 -0500

    add UnivariateAntiderivativeInfo

---
+++ src/MathEngine.CalcPlugin/AntiderivativeInfo.cs
@@ -0,0 +1,29 @@
+﻿using MathEngine.Algebra;
+using MathEngine.Algebra.Expressions;
+using MathEngine.Functions;
+
+namespace MathEngine.CalcPlugin;
+
+public abstract class UnivariateAntiderivativeInfo
+{
+	public bool TryGetWrtLinearArgument(FunctionExpression function, Variable wrt, out Expression antiderivative)
+	{
+		if (function.Args.Length != 1)
+		{
+			antiderivative = default!;
+			return false;
+		}
+		
+		var arg = function.Args[0];
+
+		if (arg != wrt)
+		{
+			antiderivative = default!;
+			return false;
+		}
+
+		return TryGetAntiderivative(function.FuncName, wrt, out antiderivative);
+	}
+
+	protected abstract bool TryGetAntiderivative(string funcName, Variable wrt, out Expression antiderivative);
+}
