d5ef252970968cbe1fc998433314a6bcebb88a3e
User0332 <carlzfurtado@gmail.com>
2025-03-04 10:59:41 -0500

    add integration base

---
+++ src/MathEngine.Calculus.Univariate/Integral/IntegrationApplications.cs
@@ -0,0 +1,27 @@
+using MathEngine.Algebra;
+using MathEngine.Algebra.Expressions;
+using MathEngine.Functions;
+
+namespace MathEngine.Calculus.Univariate.Integral;
+
+public static class IntegrationApplicationExtensions
+{
+	public static Expression AreaUnderCurve(this Integrator integrator, Expression func, Expression a, Expression b, Variable wrt)
+	{
+		var antiDeriv = integrator.Integrate(func, wrt);
+
+		return antiDeriv.SubstituteVariable(wrt, b) - antiDeriv.SubstituteVariable(wrt, a);
+	}
+
+	public static Expression AverageValueOfFunction(this Integrator integrator, Expression func, Expression a, Expression b, Variable wrt)
+	{
+		return integrator.AreaUnderCurve(func, a, b, wrt) / (b - a);
+	}
+
+	public static Expression DiscMethod(this Integrator integrator, Expression func, Expression a, Expression b, Variable wrt)
+	{
+		return Expression.PI*integrator.AreaUnderCurve(func * func, a, b, wrt);
+	}
+
+	
+}
---
+++ src/MathEngine.Calculus.Univariate/Integral/Integrator.cs
@@ -0,0 +1,49 @@
+﻿using MathEngine.Algebra;
+using MathEngine.Algebra.Expressions;
+using MathEngine.Algebra.Expressions.Operational;
+using MathEngine.CalcPlugin;
+using MathEngine.Functions;
+
+namespace MathEngine.Calculus.Univariate.Integral;
+
+public sealed class Integrator
+{
+	public List<UnivariateAntiderivativeInfo> integrators = [];
+
+	public void AddPlugin<T>() where T: UnivariateAntiderivativeInfo, new()
+	{
+		if (IsUsingPlugin<T>()) return;
+		integrators.Add(new T());
+	}
+
+	public void RemovePlugin<T>() where T: UnivariateAntiderivativeInfo
+	{
+		foreach (var plugin in integrators)
+		{
+			if (plugin is T)
+			{
+				integrators.Remove(plugin);
+				return;
+			}
+		}
+	}
+
+	public bool IsUsingPlugin<T>() where T: UnivariateAntiderivativeInfo
+	{
+		return integrators.Any(x => x is T);
+	}
+
+
+	public Expression Integrate(Expression expression, Variable wrt)
+	{
+		expression = expression.Simplify();
+
+		throw new ArgumentException($"unable to take antiderivative of {expression} wrt {wrt}");
+	}
+	
+
+	Expression IntegrateProduct(ProductExpression expression, Variable wrt)
+	{
+		return Expression.Undefined;
+	}
+}
